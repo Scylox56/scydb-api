37830c28009b7dfb3746e5abd57c1678f61439a0
Scylox56 <bodykhaled06@gmail.com>
2025-07-19 21:19:23 +0300

    chore(config): implement MongoDB connection handler with error logging

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const DB_URI = process.env.DB_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB Connected');
+  } catch (err) {
+    console.error('❌ MongoDB Connection Error:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = { DB_URI, connectDB};
