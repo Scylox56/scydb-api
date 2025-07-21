3ffc0c8412f072e0e1ac269e7eba9e4856fcc14a
Scylox56 <bodykhaled06@gmail.com>
2025-07-21 17:43:52 +0300

    feat: Implement review routes with authentication protection and client restrictions

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,3 +2,17 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
+
+router.use(authController.protect);
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(authController.restrictTo('client'), reviewController.createReview);
+
+
+router
+  .route('/:id')
+  .delete(reviewController.deleteReview);
+
+module.exports = router;  
