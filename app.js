bcabe21c10c2cec68c3ec02370bf7429f3d78e07
Scylox56 <bodykhaled06@gmail.com>
2025-08-04 16:53:22 +0300

    fix: linking to website

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(cors({
     'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://scydb.netlify.app'
   ],
   credentials: true
 }));
